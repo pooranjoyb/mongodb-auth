5843109a28590bb6f2a32d5ace82dcc0fb9cb4bb
pooranjoyb <pooranjoyb2016@gmail.com>
2023-01-10 23:24:02 +0530

    Setup ejs

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,37 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import bodyparser from'body-parser'
+import session from 'express-session'
+import { uuid } from 'uuidv4'
 
 dotenv.config();
 
-const PORT = process.env.PORT;
 const app = express();
+const PORT = process.env.PORT;
+
+//setting up viewport
+app.set('view engine', 'ejs');
 
 app.use(cors());
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}))
+
+
+//loading static assets
 app.use(express.static('./public'));
+
+app.use(session({
+    secret: uuid(),
+    resave: false,
+    saveUninitialized: true
+}));
+
+//home-route
+app.get('/', (req, res) => {
+    res.render('../public/views/base', {title: "Netflix"})
+})
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
